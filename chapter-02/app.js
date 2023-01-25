c4a7104401565143eb3b8cf777c939dca0837c50
manish verma <manishverma@manishs-MacBook-Pro.local>
2023-01-25 23:24:21 +0530

    chapter 03

--- chapter-02/app.js
+++ chapter-02/app.js
@@ -2,23 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading1 = React.createElement(
-  "h1",
-  { id: "heading1", className: "bg-danger" },
-  "Heading 1"
+
+//* React.createElement => Object => HTML(DOM)
+//* JSX ??
+
+// React Element
+const heading = (
+  <h1 id="heading" className="bg-danger" key="h" tabIndex="1">
+    Heading
+  </h1>
 );
-const heading2 = React.createElement(
-  "h2",
-  { id: "heading2", className: "bg-primary" },
-  "Heading 2"
+
+// React Components
+// - Functional - NEW
+// Name start with capital letter
+// Functional component is a normal function and returns react element
+// const HeaderComponent1 = () => { return react elements };
+// - Class Based Component - OLDß
+
+const HeaderComponent1 = () => {
+  return <h1>Namaste React functional Component</h1>;
+};
+
+const HeaderComponent2 = () => (
+  <div>
+    {/* any js syntax we can write */}
+    {1 + 2}
+    {heading}
+    {HeaderComponent1()}
+    <HeaderComponent1 />
+    <h1>Namaste React functional Component</h1>
+    <h2>This is h2 tags</h2>
+  </div>
 );
 
-//* we can put anything inside props
-// const container = React.createElement('element',{props}, 'text')
+root.render(<HeaderComponent2 />); // react component
 
-const container = React.createElement("div", { className: "container" }, [
-  heading1,
-  heading2,
-]);
-
-root.render(container);
+// Cross Side Scripting Attack
+// JS JSX is very scure
+// {any js code we put it santize it}
